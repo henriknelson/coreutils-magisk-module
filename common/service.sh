775f4bbdd84854a064fdfe7fa913b4c376a70a0d
Henrik Nelson <henrik@cliffords.nu>
2020-10-14 00:58:58 +0200

    Cleaned up in coreutils module

--- common/service.sh
+++ common/service.sh
@@ -6,23 +6,23 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*};
 
-symlink_from_file() {
-   cat $1 | while read line
-   do
-      target=$(echo $line | sed 's/;/ /g' | awk '{printf $2}');
-      symlink=$(echo $line | sed 's/;/ /g' | awk '{printf $1}');
-      ln -sf $target $2/$symlink;
-      chown 0:0 $2/$symlink;
-      chmod 755 $2/$symlink;
-   done
-   chown -R 0:0 $2;
-   chmod -R 755 $2;
-}
+#symlink_from_file() {
+#   cat $1 | while read line
+#   do
+#      target=$(echo $line | sed 's/;/ /g' | awk '{printf $2}');
+#      symlink=$(echo $line | sed 's/;/ /g' | awk '{printf $1}');
+#      ln -sf $target $2/$symlink;
+#      chown 0:0 $2/$symlink;
+#      chmod 755 $2/$symlink;
+#   done
+#   chown -R 0:0 $2;
+#   chmod -R 755 $2;
+#}
 
-mount -o rw,remount /system/bin;
+#mount -o rw,remount /system/bin;
 
-cd /system/bin;
-cp $MODDIR/symlinks/symlinks_coreutils .;
-symlink_from_file "$(pwd)/symlinks_coreutils" "$(pwd)";
+#cd /system/bin;
+#cp $MODDIR/symlinks/symlinks_coreutils .;
+#symlink_from_file "$(pwd)/symlinks_coreutils" "$(pwd)";
 
-mount -o ro,remount /system/bin;
+#mount -o ro,remount /system/bin;
